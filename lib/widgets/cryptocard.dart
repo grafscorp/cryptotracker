a1965750a3f7ad5b548a072cdee0d8028d30c907
Grafscorp <golyandringraf@gmail.com>
2024-12-08 21:25:25 +0300

    add CoinChart to CryptoCard

--- lib/widgets/cryptocard.dart
+++ lib/widgets/cryptocard.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cryptotracker/models/cryptodata.dart';
+import 'package:cryptotracker/widgets/coinchart.dart';
 import 'package:cryptotracker/widgets/coinimage.dart';
 import 'package:flutter/material.dart';
 
@@ -17,12 +18,13 @@ class Cryptocard extends StatelessWidget {
       height: 100,
       child: Card(
         child: Row(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            //Label Coin and Charts
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -30,6 +32,8 @@ class Cryptocard extends StatelessWidget {
                   padding: const EdgeInsets.only(right: 10, left: 20),
                   child: CoinImage(imageUrl: cryptoData.imageUrl, size: 40),
                 ),
+
+                //Text Label Coin
                 SizedBox(
                   width: 80,
                   child: Column(
@@ -49,20 +53,29 @@ class Cryptocard extends StatelessWidget {
                         overflow: TextOverflow.ellipsis,
                         style: const TextStyle(fontSize: 16),
                       ),
+                      //TEST
                     ],
                   ),
                 ),
+                //ChartsCoin
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: SizedBox(width: 100, height: 60, child: CoinChart()),
+                ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: Container(
-                width: 100.0,
-                color: Colors.red,
-              ),
-            ),
+            //Chart Coin
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            //   child: Container(
+            //     width: 100.0,
+            //     color: Colors.red,
+            //   ),
+            // ),
+            //Coin Data
             Row(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisSize: MainAxisSize.max,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(right: 10),
